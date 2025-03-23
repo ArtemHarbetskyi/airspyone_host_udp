d434b3386a11e9af3c0318d45a2bdbac29adcfb8
A.Harbetskyi <artem.kmcp@gmail.com>
2025-03-23 11:35:44 +0200

    Update airspy_rx.c

--- airspy-tools/src/airspy_rx.c
+++ airspy-tools/src/airspy_rx.c
@@ -34,6 +34,12 @@
 #include <errno.h>
 #include <limits.h>
 
+#include <arpa/inet.h>  // Для UDP
+#include <sys/time.h>
+
+#include <pthread.h>
+#include <arpa/inet.h>
+
 #define AIRSPY_RX_VERSION "1.0.5 23 April 2016"
 
 #ifndef bool
@@ -55,6 +61,17 @@ typedef int32_t ssize_t;
 
 #define strtoull _strtoui64
 #define snprintf _snprintf
+
+// Глобальные переменные для UDP
+int udp_sock;
+struct sockaddr_in udp_client;
+socklen_t client_len = sizeof(udp_client);
+
+// Переменные для IP и порта (можно менять)
+char udp_ip[] = "10.5.0.4";  // <-- IP отримувача
+int udp_port = 1236;              // <-- Порт отримувача
+
+
 
 int gettimeofday(struct timeval *tv, void* ignored)
 {
@@ -359,110 +376,139 @@ char* u64toa(uint64_t val, t_u64toa* str)
 	return res;
 }
 
+void* udp_server(void* arg)
+{
+    int server_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[64];
+    socklen_t addr_len = sizeof(client_addr);
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(9000);  // Порт для команд
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    while (!do_exit) {
+        recvfrom(server_sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&client_addr, &addr_len);
+        buffer[63] = '\0';  // Защита от переполнения
+
+        if (strncmp(buffer, "SET FREQ ", 9) == 0) {
+            int freq = atoi(buffer + 9);
+            printf("Setting frequency to %d Hz\n", freq);
+            airspy_set_freq(device, freq);
+        }
+    }
+    close(server_sock);
+    return NULL;
+}
+
+void setup_udp()
+{
+    udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
+    udp_client.sin_family = AF_INET;
+    udp_client.sin_port = htons(udp_port);
+    inet_pton(AF_INET, udp_ip, &udp_client.sin_addr);
+}
+
 int rx_callback(airspy_transfer_t* transfer)
 {
-	uint32_t bytes_to_write;
-	void* pt_rx_buffer;
-	ssize_t bytes_written;
-	struct timeval time_now;
-	float time_difference, rate;
+    uint32_t bytes_to_write;
+    void* pt_rx_buffer;
+    struct timeval time_now;
+    float time_difference, rate;
 
-	if( fd != NULL ) 
-	{
-		switch(sample_type_val)
-		{
-			case AIRSPY_SAMPLE_FLOAT32_IQ:
-				bytes_to_write = transfer->sample_count * FLOAT32_EL_SIZE_BYTE * 2;
-				pt_rx_buffer = transfer->samples;
-				break;
+    // Определяем размер данных в зависимости от типа выборки
+    switch(sample_type_val)
+    {
+        case AIRSPY_SAMPLE_FLOAT32_IQ:
+            bytes_to_write = transfer->sample_count * FLOAT32_EL_SIZE_BYTE * 2;
+            pt_rx_buffer = transfer->samples;
+            break;
 
-			case AIRSPY_SAMPLE_FLOAT32_REAL:
-				bytes_to_write = transfer->sample_count * FLOAT32_EL_SIZE_BYTE * 1;
-				pt_rx_buffer = transfer->samples;
-				break;
+        case AIRSPY_SAMPLE_FLOAT32_REAL:
+            bytes_to_write = transfer->sample_count * FLOAT32_EL_SIZE_BYTE * 1;
+            pt_rx_buffer = transfer->samples;
+            break;
 
-			case AIRSPY_SAMPLE_INT16_IQ:
-				bytes_to_write = transfer->sample_count * INT16_EL_SIZE_BYTE * 2;
-				pt_rx_buffer = transfer->samples;
-				break;
+        case AIRSPY_SAMPLE_INT16_IQ:
+            bytes_to_write = transfer->sample_count * INT16_EL_SIZE_BYTE * 2;
+            pt_rx_buffer = transfer->samples;
+            break;
 
-			case AIRSPY_SAMPLE_INT16_REAL:
-				bytes_to_write = transfer->sample_count * INT16_EL_SIZE_BYTE * 1;
-				pt_rx_buffer = transfer->samples;
-				break;
+        case AIRSPY_SAMPLE_INT16_REAL:
+            bytes_to_write = transfer->sample_count * INT16_EL_SIZE_BYTE * 1;
+            pt_rx_buffer = transfer->samples;
+            break;
 
-			case AIRSPY_SAMPLE_UINT16_REAL:
-				bytes_to_write = transfer->sample_count * INT16_EL_SIZE_BYTE * 1;
-				pt_rx_buffer = transfer->samples;
-				break;
+        case AIRSPY_SAMPLE_UINT16_REAL:
+            bytes_to_write = transfer->sample_count * INT16_EL_SIZE_BYTE * 1;
+            pt_rx_buffer = transfer->samples;
+            break;
 
-			case AIRSPY_SAMPLE_RAW:
-				if (packing_val)
-				{
-					bytes_to_write = transfer->sample_count * INT12_EL_SIZE_BITS / INT8_EL_SIZE_BITS;
-				}
-				else
-				{
-					bytes_to_write = transfer->sample_count * INT16_EL_SIZE_BYTE * 1;
-				}
-				pt_rx_buffer = transfer->samples;
-				break;
+        case AIRSPY_SAMPLE_RAW:
+            if (packing_val)
+            {
+                bytes_to_write = transfer->sample_count * INT12_EL_SIZE_BITS / INT8_EL_SIZE_BITS;
+            }
+            else
+            {
+                bytes_to_write = transfer->sample_count * INT16_EL_SIZE_BYTE * 1;
+            }
+            pt_rx_buffer = transfer->samples;
+            break;
 
-			default:
-				bytes_to_write = 0;
-				pt_rx_buffer = NULL;
-			break;
-		}
+        default:
+            bytes_to_write = 0;
+            pt_rx_buffer = NULL;
+            break;
+    }
 
-		gettimeofday(&time_now, NULL);
+    // Обновляем время и статистику
+    gettimeofday(&time_now, NULL);
+    if (!got_first_packet)
+    {
+        t_start = time_now;
+        time_start = time_now;
+        got_first_packet = true;
+    }
+    else
+    {
+        buffer_count++;
+        sample_count += transfer->sample_count;
+        if (buffer_count == 50)
+        {
+            time_difference = TimevalDiff(&time_now, &time_start);
+            rate = (float) sample_count / time_difference;
+            average_rate += 0.2f * (rate - average_rate);
+            global_average_rate += average_rate;
+            rate_samples++;
+            time_start = time_now;
+            sample_count = 0;
+            buffer_count = 0;
+        }
+    }
 
-		if (!got_first_packet)
-		{
-			t_start = time_now;
-			time_start = time_now;
-			got_first_packet = true;
-		}
-		else
-		{
-			buffer_count++;
-			sample_count += transfer->sample_count;
-			if (buffer_count == 50)
-			{
-				time_difference = TimevalDiff(&time_now, &time_start);
-				rate = (float) sample_count / time_difference;
-				average_rate += 0.2f * (rate - average_rate);
-				global_average_rate += average_rate;
-				rate_samples++;
-				time_start = time_now;
-				sample_count = 0;
-				buffer_count = 0;
-			}
-		}
+    // Контроль передачи ограниченного количества сэмплов
+    if (limit_num_samples) {
+        if (bytes_to_write >= bytes_to_xfer) {
+            bytes_to_write = (int)bytes_to_xfer;
+        }
+        bytes_to_xfer -= bytes_to_write;
+    }
 
-		if (limit_num_samples) {
-			if (bytes_to_write >= bytes_to_xfer) {
-				bytes_to_write = (int)bytes_to_xfer;
-			}
-			bytes_to_xfer -= bytes_to_write;
-		}
+    // Передача данных через UDP
+    if (pt_rx_buffer != NULL)
+    {
+        sendto(udp_sock, pt_rx_buffer, bytes_to_write, 0, 
+               (struct sockaddr*)&udp_client, client_len);
+    }
 
-		if(pt_rx_buffer != NULL)
-		{
-			bytes_written = fwrite(pt_rx_buffer, 1, bytes_to_write, fd);
-		}else
-		{
-			bytes_written = 0;
-		}
-		if ( (bytes_written != bytes_to_write) || 
-				 ((limit_num_samples == true) && (bytes_to_xfer == 0)) 
-				)
-			return -1;
-		else
-			return 0;
-	}else
-	{
-		return -1;
-	}
+    // Остановка потока при достижении лимита
+    if ((limit_num_samples == true) && (bytes_to_xfer == 0)) 
+        return -1;
+    else
+        return 0;
 }
 
 static void usage(void)
@@ -1019,6 +1065,11 @@ int main(int argc, char** argv)
 		}
 	}
 
+	// udp server start
+	pthread_t udp_thread;
+	pthread_create(&udp_thread, NULL, udp_server, NULL);
+	pthread_detach(udp_thread);  // Автоматически завершать поток
+	
 	result = airspy_start_rx(device, rx_callback, NULL);
 	if( result != AIRSPY_SUCCESS ) {
 		fprintf(stderr, "airspy_start_rx() failed: %s (%d)\n", airspy_error_name(result), result);
